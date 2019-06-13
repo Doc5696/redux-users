8aac717c204fd795d6350c1bf9d251696c7a7c6b
Doc5696 <root.itbox@gmail.com>
2019-06-13 10:20:20 +0300

    requests to api with axios

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import axios from 'axios';
 import rootReducer from './reducers';
 
 const store = createStore(
@@ -8,5 +9,14 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(thunk))
 );
 
+axios.interceptors.request.use(
+  function(config) {
+    config.baseURL = "http://localhost:8081/api";
+    return config;
+  },
+  function(err) {
+    return Promise.reject(err);
+  }
+);
 
 export default store;
